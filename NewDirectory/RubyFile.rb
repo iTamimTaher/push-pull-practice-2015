1f22ea7a064a9b40e0a537cde055f4360ff13956
itamimtaher99 <tamimtaher2@gmail.com>
2015-11-16 18:23:35 +0000

    Hey Mr. Prem!

---
+++ NewDirectory/RubyFile.rb
@@ -0,0 +1,11 @@
+ToDoList = ["Wake up at 5:00 AM", "Brush my teeth", "Take a shower", "Eat breakfast", "Put on some pants"]
+
+puts "Today, I have #{ToDoList.length} things to do."
+
+puts "When you square the number 5, you get #{5 * 5}."
+
+puts "When you add 5 to #{5 + 5}, you get #{5 * 3}."
+
+
+
+    
